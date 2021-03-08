b61517d228324ebc1330b45e0f433baddd8e9886
Ezekiel Wheeler <kj7nll@gmail.com>
2021-03-07 16:10:06 -0800

    testing gpio

---
+++ main.c
@@ -0,0 +1,72 @@
+//EFM32 blink test
+
+#ifndef LED_PIN
+#define LED_PIN     0
+#endif
+#ifndef LED_PORT
+#define LED_PORT    gpioPortB
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_gpio.h"
+
+volatile uint32_t msTicks; /* counts 1ms timeTicks */
+
+void Delay(uint32_t dlyTicks);
+
+/**************************************************************************//**
+ * @brief SysTick_Handler
+ * Interrupt Service Routine for system tick counter
+ *****************************************************************************/
+void SysTick_Handler(void)
+{
+    msTicks++;       /* increment counter necessary in Delay()*/
+}
+
+/**************************************************************************//**
+ * @brief Delays number of msTick Systicks (typically 1 ms)
+ * @param dlyTicks Number of ticks to delay
+ *****************************************************************************/
+void Delay(uint32_t dlyTicks)
+{
+    uint32_t curTicks;
+
+    curTicks = msTicks;
+    while ((msTicks - curTicks) < dlyTicks) ;
+}
+
+/**************************************************************************//**
+ * @brief  Main function
+ *****************************************************************************/
+int main(void)
+{
+    CHIP_Init();
+
+    CMU_ClockEnable(cmuClock_GPIO, true);
+
+    /* Setup SysTick Timer for 1 msec interrupts  */
+    if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
+
+    /* Initialize LED driver */
+    GPIO_PinModeSet(LED_PORT, LED_PIN, gpioModePushPull, 0);
+
+    GPIO_PinOutSet(LED_PORT, LED_PIN);
+
+    printf("test");
+
+    /* Infinite blink loop */
+    while (1)
+    {
+        GPIO_PinOutToggle(LED_PORT, LED_PIN);
+        Delay(2000);
+    }
+}
+
+
+
