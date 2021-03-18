630eae6d53778fb352c9a61c8849f4e6ceafaeb7
Ezekiel Wheeler <kj7nll@gmail.com>
2021-03-17 21:02:47 -0700

    Use the print function

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "em_device.h"
 #include "em_chip.h"
 #include "em_emu.h"
@@ -13,7 +15,9 @@
 #define LED1_PIN 1
 
 // Receive data buf
-uint8_t buf[BUFLEN];
+uint8_t bufrx[BUFLEN];
+uint8_t *buftx = NULL;
+int buftxlen = 0, bufrxidx = 0;
 
 
 
@@ -71,7 +75,7 @@ void initUsart1(void)
 void USART1_RX_IRQHandler(void)
 {
 	// Get the character just received
-	buf[0] = USART1->RXDATA;
+	// buf[0] = USART1->RXDATA;
 
 	GPIO_PinOutToggle(LED_PORT, LED0_PIN);
 	GPIO_PinOutToggle(LED_PORT, LED1_PIN);
@@ -81,16 +85,34 @@ void USART1_RX_IRQHandler(void)
 
 void USART1_TX_IRQHandler(void)
 {
-	if (buf[0])
-		USART1->TXDATA = buf[0];
-	buf[0] = 0;
+	if (buftx != NULL && buftxlen > 0)
+	{
+		USART1->TXDATA = *buftx;
+		buftx++;
+		buftxlen--;
 
-	/*
-	 * Need to disable the transmit buf level interrupt in this IRQ
-	 * handler when done or it will immediately trigger again upon exit
-	 * even though there is no data left to send.
-	 */
-	USART_IntDisable(USART1, USART_IEN_TXBL);
+		if (buftxlen == 0)
+		{
+			buftx = NULL;
+
+			/*
+			 * Need to disable the transmit buf level interrupt in this IRQ
+			 * handler when done or it will immediately trigger again upon exit
+			 * even though there is no data left to send.
+			 */
+			USART_IntDisable(USART1, USART_IEN_TXBL);
+		}
+	}
+}
+
+void print(uint8_t *s, int len)
+{
+	buftx = s;
+	buftxlen = len;
+	USART_IntEnable(USART1, USART_IEN_TXBL);
+	while (buftx != NULL)
+		EMU_EnterEM1();	
+
 }
 
 int main(void)
@@ -108,6 +130,8 @@ int main(void)
 	// Enable receive data valid interrupt
 	USART_IntEnable(USART1, USART_IEN_RXDATAV);
 
+
+	print("Hello World", 11);
 	// Enable transmit buffer level interrupt
 	// USART_IntEnable(USART1, USART_IEN_TXBL);
 	while (1)
