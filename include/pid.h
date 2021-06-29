647813d8456ceef1530da2cee0d480406f4e9cb3
Ezekiel Wheeler <kj7nll@gmail.com>
2021-06-28 20:01:08 -0700

    Added PID controller

---
+++ include/pid.h
@@ -0,0 +1,41 @@
+#ifndef PID_CONTROLLER_H
+#define PID_CONTROLLER_H
+
+typedef struct
+{
+	// Controller gains 
+	float Kp;
+	float Ki;
+	float Kd;
+
+	
+	// Derivative low-pass filter time constant 
+	float tau;
+
+	// Output limits 
+	float out_min;
+	float out_max;
+
+	// Integrator limits
+	float int_min;
+	float int_max;
+
+	// Sample time (in seconds) 
+	float T;
+
+	// Controller "memory" 
+	float integrator;
+	float prevError;	/* Required for integrator */
+
+	float differentiator;
+	float prevMeasurement;	/* Required for differentiator */
+
+	// Controller output 
+	float out;
+
+} PIDController;
+
+void PIDController_Init(PIDController *pid);
+float PIDController_Update(PIDController *pid, float setpoint, float measurement);
+
+#endif
---
+++ pid.c
@@ -0,0 +1,50 @@
+#include "pid.h"
+
+void PIDController_Init(PIDController *pid)
+{
+	pid->integrator = 0.0f;
+	pid->prevError = 0.0f;
+
+	pid->differentiator = 0.0f;
+	pid->prevMeasurement = 0.0f;
+
+	pid->out = 0.0f;
+}
+
+float PIDController_Update(PIDController *pid, float setpoint, float measurement)
+{
+
+	float error = setpoint - measurement;
+	float proportional = pid->Kp * error;
+
+	pid->integrator =
+		pid->integrator + 0.5f * pid->Ki * pid->T * (error +
+							     pid->prevError);
+
+	if (pid->integrator > pid->int_max)
+		pid->integrator = pid->int_max;
+	else if (pid->integrator < pid->int_min)
+		pid->integrator = pid->int_min;
+
+	// Derivative (band-limited differentiator)
+	// Note: derivative on measurement, therefore minus sign in front of equation!
+	pid->differentiator = -(2.0f * pid->Kd * (measurement - pid->prevMeasurement) 
+					+ (2.0f * pid->tau - pid->T) * pid->differentiator)
+				/ (2.0f * pid->tau + pid->T);
+
+	// Compute output and apply limits
+	pid->out = proportional + pid->integrator + pid->differentiator;
+
+	if (pid->out > pid->out_max)
+		pid->out = pid->out_max;
+	else if (pid->out < pid->out_min)
+		pid->out = pid->out_min;
+
+	// Store error and measurement for later use 
+	pid->prevError = error;
+	pid->prevMeasurement = measurement;
+
+	// Return controller output 
+	return pid->out;
+
+}
