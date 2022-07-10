50c99d5e3bd2ec93ce870526dffed7af6c075858
Zeke, KJ7NLL <kj7nll@gmail.com>
2022-07-10 15:12:03 -0700

    Added licensing header and related files

--- include/pwm.h
+++ include/pwm.h
@@ -1,3 +1,23 @@
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL and Eric Wheeler, KJ7LNW.
+//  All rights reserved.
+//
+//  The official website and doumentation for space-ham is available here:
+//    https://www.kj7nll.radio/
+//
 int timer_idx(TIMER_TypeDef *timer);
 
 void timer_init_pwm(TIMER_TypeDef *timer, int cc, int port, int pin, int pwm_Hz, float duty_cycle);
--- pwm.c
+++ pwm.c
@@ -1,3 +1,23 @@
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL and Eric Wheeler, KJ7LNW.
+//  All rights reserved.
+//
+//  The official website and doumentation for space-ham is available here:
+//    https://www.kj7nll.radio/
+//
 #include "em_device.h"
 #include "em_cmu.h"
 #include "em_emu.h"
