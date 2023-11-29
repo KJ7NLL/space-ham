3ed62df7d2fd3b2129c89a65becd0d1f37d5b048
Zeke, KJ7NLL <kj7nll@gmail.com>
2023-11-28 23:07:51 -0800

    relicense i2c.c
    We have completely rewritten i2c to be interrupt handler driven. Since it is
    no longer a Silicon Labs example, we are changing the license to GPL.

--- i2c.c
+++ i2c.c
@@ -1,25 +1,23 @@
-/*
- * SPDX-License-Identifier: Zlib
- * 
- * The licensor of this software is Silicon Laboratories Inc.
- * 
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * 
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
-
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL and Eric Wheeler, KJ7LNW.
+//  All rights reserved.
+//
+//  The official website and doumentation for space-ham is available here:
+//    https://www.kj7nll.radio/
+//
 #include <stdio.h>
 #include <string.h>
 
@@ -139,6 +137,8 @@ void I2C0_IRQHandler()
 	}
 }
 
+// This function is mostly coppied from a Silicon Labs example. If anyone
+// cares, this function may still be licensed as zlib.
 void initI2C()
 {
 	CMU_ClockEnable(cmuClock_I2C0, true);
