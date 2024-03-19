59fae611a54a3c91c969805be39c59cc72c723f2
Zeke, KJ7NLL <kj7nll@gmail.com>
2024-03-18 23:46:00 -0700

    Added support for the mmc5603nj compass

---
+++ main/i2c/mmc5603nj.c
@@ -0,0 +1,280 @@
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL and Eric Wheeler, KJ7LNW.
+//  All rights reserved.
+//
+//  The official website and doumentation for space-ham is available here:
+//    https://www.kj7nll.radio/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "i2c.h"
+#include "i2c/mmc5603nj.h"
+
+void mmc5603nj_init(mmc5603nj_t *mag)
+{
+	// Initializes max value to the smallest possible value to
+	// ensure that the mins and maxes are changed to proper values
+	mag->min_x = 65535;
+	mag->max_x = 0;
+	mag->min_y = 65535;
+	mag->max_y = 0;
+	mag->min_z = 65535;
+	mag->max_z = 0;
+}
+
+void mmc5603nj_config_write(mmc5603nj_t *mag)
+{
+	uint8_t data[4];
+	uint16_t devaddr = mag->req.addr >> 1;
+
+	data[0] = (mag->control_reg_odr << 0);
+
+	data[1] = (mag->control_reg_0_take_meas_m << 0) |
+		  (mag->control_reg_0_take_meas_t << 1) |
+		  (mag->control_reg_0_start_mdt << 2) |
+		  (mag->control_reg_0_do_set << 3) |
+		  (mag->control_reg_0_do_reset << 4) |
+		  (mag->control_reg_0_auto_sr_en << 5) |
+		  (mag->control_reg_0_auto_st_en << 6) |
+		  (mag->control_reg_0_cmm_freq_en << 7);
+
+	data[2] = (mag->control_reg_1_bw << 0) |
+		  (mag->control_reg_1_x_inhibit << 2) |
+		  (mag->control_reg_1_y_inhibit << 3) |
+		  (mag->control_reg_1_z_inhibit << 4) |
+		  (mag->control_reg_1_st_enp << 5) |
+		  (mag->control_reg_1_st_enm << 6) |
+		  (mag->control_reg_1_sw_reset << 7);
+
+	data[3] = (mag->control_reg_2_prd_set << 0) |
+		  (mag->control_reg_2_en_prd_set << 3) |
+		  (mag->control_reg_2_cmm_en << 4) |
+		  (mag->control_reg_2_int_mdt_en << 5) |
+		  (mag->control_reg_2_int_meas_done_en << 6) |
+		  (mag->control_reg_2_hpower << 7);
+
+	printf("mmc5603nj config: %x: %x\r\n", devaddr, data[0]);
+	printf("mmc5603nj config: %x: %x\r\n", devaddr, data[1]);
+	printf("mmc5603nj config: %x: %x\r\n", devaddr, data[2]);
+	printf("mmc5603nj config: %x: %x\r\n", devaddr, data[3]);
+	i2c_master_write(devaddr << 1, MMC5603NJ_CONTROL_REG_ODR, data, 4);
+}
+
+uint16_t mmc5603nj_measure_req_raw(mmc5603nj_t *mag, uint8_t data_reg)
+{
+	uint8_t *data = mag->req.result;
+
+	if (data == NULL || !mag->req.valid)
+		return 0;
+
+	return (data[data_reg+0] << 8) | data[data_reg+1];
+}
+
+uint16_t mmc5603nj_measure_req_avg(uint16_t *arr)
+{
+	uint32_t avg = 0;
+
+	int i;
+	
+	for (i = 0; i < MMC5603NJ_SAMPLE_AVG; i++)
+	{
+		avg += (uint32_t)arr[i];
+	}
+
+	avg /= MMC5603NJ_SAMPLE_AVG;
+
+	return (uint16_t)avg;
+}
+
+float mmc5603nj_measure_req(mmc5603nj_t *mag, uint8_t data_reg)
+{
+	uint16_t value = 0;
+	uint16_t min, max;
+
+	if (data_reg == MMC5603NJ_DATA_X)
+	{
+		value = mmc5603nj_measure_req_avg(mag->x);
+		min = mag->min_x;
+		max = mag->max_x;
+	}
+	else if (data_reg == MMC5603NJ_DATA_Y)
+	{
+		value = mmc5603nj_measure_req_avg(mag->y);
+		min = mag->min_y;
+		max = mag->max_y;
+	}
+	else if (data_reg == MMC5603NJ_DATA_Z)
+	{
+		value = mmc5603nj_measure_req_avg(mag->z);
+		min = mag->min_z;
+		max = mag->max_z;
+	}
+	else
+		return NAN;
+
+	
+	return ((1.0 / (max - min)) * (value - min)) * 2 - 1;
+}
+
+float mmc5603nj_measure_req_plane(mmc5603nj_t *mag, uint8_t plane)
+{
+	float a, b;
+	bool t;
+
+	if (plane == MMC5603NJ_PLANE_XY)
+	{
+		a = mmc5603nj_measure_req(mag, MMC5603NJ_DATA_X);
+		b = mmc5603nj_measure_req(mag, MMC5603NJ_DATA_Y);
+
+		if (mag->invert_x)
+			a = -a;
+
+		if (mag->invert_y)
+			b = -b;
+
+		if (mag->swap_xy)
+		{
+			t = a;
+			a = b;
+			b = t;
+		}
+	}
+	else if (plane == MMC5603NJ_PLANE_XZ)
+	{
+		a = mmc5603nj_measure_req(mag, MMC5603NJ_DATA_X);
+		b = mmc5603nj_measure_req(mag, MMC5603NJ_DATA_Z);
+
+		if (mag->invert_x)
+			a = -a;
+
+		if (mag->invert_z)
+			b = -b;
+
+		if (mag->swap_xz)
+		{
+			t = a;
+			a = b;
+			b = t;
+		}
+	}
+	else if (plane == MMC5603NJ_PLANE_YZ)
+	{
+		a = mmc5603nj_measure_req(mag, MMC5603NJ_DATA_Y);
+		b = mmc5603nj_measure_req(mag, MMC5603NJ_DATA_Z);
+
+		if (mag->invert_y)
+			a = -a;
+
+		if (mag->invert_z)
+			b = -b;
+
+		if (mag->swap_yz)
+		{
+			t = a;
+			a = b;
+			b = t;
+		}
+	}
+	else
+		return NAN;
+
+	return 180 + atan2(a, b) * 180 / M_PI;
+}
+
+void mmc5603nj_calibration_callback(mmc5603nj_t *mag)
+{
+	uint16_t *xp, *yp, *zp;
+	uint16_t x, y, z;
+	mag->x_idx = (mag->x_idx + 1) % MMC5603NJ_SAMPLE_AVG;
+	mag->y_idx = (mag->y_idx + 1) % MMC5603NJ_SAMPLE_AVG;
+	mag->z_idx = (mag->z_idx + 1) % MMC5603NJ_SAMPLE_AVG;
+
+	xp = &mag->x[mag->x_idx];
+	yp = &mag->y[mag->y_idx];
+	zp = &mag->z[mag->z_idx];
+
+	x = mmc5603nj_measure_req_raw(mag, MMC5603NJ_DATA_X);
+	y = mmc5603nj_measure_req_raw(mag, MMC5603NJ_DATA_Y);
+	z = mmc5603nj_measure_req_raw(mag, MMC5603NJ_DATA_Z);
+
+	if (x != 0 && x != 65535)
+	{
+		*xp = x;
+
+		if (*xp > mag->max_x)
+			mag->max_x = *xp;
+
+		if (*xp < mag->min_x)
+			mag->min_x = *xp;
+	}
+
+	if (y != 0 && y != 65535)
+	{
+		*yp = y;
+
+		if (*yp > mag->max_y)
+			mag->max_y = *yp;
+
+		if (*yp < mag->min_y)
+			mag->min_y = *yp;
+	}
+
+	if (z != 0 && z != 65535)
+	{
+		*zp = z;
+
+		if (*zp > mag->max_z)
+			mag->max_z = *zp;
+
+		if (*zp < mag->min_z)
+			mag->min_z = *zp;
+	}
+}
+
+mmc5603nj_t *mmc5603nj_measure_req_alloc(int devaddr)
+{
+	mmc5603nj_t *mag;
+
+	mag = (mmc5603nj_t *)i2c_req_alloc(sizeof(mmc5603nj_t), 6, (devaddr << 1) | 1);
+
+	if (mag == NULL)
+		return NULL;
+
+	i2c_req_t *req = &mag->req;
+
+	req->name = "mmc5603nj";
+	req->target = MMC5603NJ_DATA_X;
+	req->callback = (void (*)(i2c_req_t *))mmc5603nj_calibration_callback;
+
+	mmc5603nj_init(mag);
+
+	return mag;
+}
+
+void mmc5603nj_measure_req_free(mmc5603nj_t *mag)
+{
+	if (&mag->req == i2c_req_get_cont(mag->req.addr >> 1))
+	{
+		i2c_req_set_cont(mag->req.addr >> 1, NULL);
+	}
+
+	free(mag->req.data);
+	free(mag->req.result);
+	free(mag);
+}
---
+++ main/include/i2c/mmc5603nj.h
@@ -0,0 +1,102 @@
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL and Eric Wheeler, KJ7LNW.
+//  All rights reserved.
+//
+//  The official website and doumentation for space-ham is available here:
+//    https://www.kj7nll.radio/
+
+#define MMC5603NJ_SAMPLE_AVG 128
+
+// This structure is cast-compatible with i2c_req_t.
+typedef struct mmc5603nj
+{
+	// This must be the first entry in the struct for casting to be possible
+	i2c_req_t req;
+
+	uint8_t control_reg_odr;
+
+	uint8_t control_reg_0_take_meas_m:1;
+	uint8_t control_reg_0_take_meas_t:1;
+	uint8_t control_reg_0_start_mdt:1;
+	uint8_t control_reg_0_do_set:1;
+	uint8_t control_reg_0_do_reset:1;
+	uint8_t control_reg_0_auto_sr_en:1;
+	uint8_t control_reg_0_auto_st_en:1;
+	uint8_t control_reg_0_cmm_freq_en:1;
+
+	uint8_t control_reg_1_bw:2;
+	uint8_t control_reg_1_x_inhibit:1;
+	uint8_t control_reg_1_y_inhibit:1;
+	uint8_t control_reg_1_z_inhibit:1;
+	uint8_t control_reg_1_st_enp:1;
+	uint8_t control_reg_1_st_enm:1;
+	uint8_t control_reg_1_sw_reset:1;
+
+	uint8_t control_reg_2_prd_set:3;
+	uint8_t control_reg_2_en_prd_set:1;
+	uint8_t control_reg_2_cmm_en:1;
+	uint8_t control_reg_2_int_mdt_en:1;
+	uint8_t control_reg_2_int_meas_done_en:1;
+	uint8_t control_reg_2_hpower:1;
+
+	int8_t  x_idx, y_idx, z_idx; 
+	uint16_t min_x, max_x;
+	uint16_t min_y, max_y;
+	uint16_t min_z, max_z;
+
+	uint16_t x[MMC5603NJ_SAMPLE_AVG], y[MMC5603NJ_SAMPLE_AVG], z[MMC5603NJ_SAMPLE_AVG];
+	bool swap_xy;
+	bool swap_xz;
+	bool swap_yz;
+	bool invert_y;
+	bool invert_x;
+	bool invert_z;
+} mmc5603nj_t;
+
+enum {
+	MMC5603NJ_DATA_X = 0x00,
+	MMC5603NJ_DATA_Y = 0x02,
+	MMC5603NJ_DATA_Z = 0x04,
+	MMC5603NJ_REG_ODR = 0x1A,
+	MMC5603NJ_CONTROL_REG_ODR = 0x1A,
+
+	MMC5603NJ_BW_75HZ = 0,
+	MMC5603NJ_BW_150HZ,
+	MMC5603NJ_BW_255HZ,
+	MMC5603NJ_BW_1000HZ,
+
+	MMC5603NJ_PRD_SET_1 = 0,
+	MMC5603NJ_PRD_SET_25,
+	MMC5603NJ_PRD_SET_75,
+	MMC5603NJ_PRD_SET_100,
+	MMC5603NJ_PRD_SET_250,
+	MMC5603NJ_PRD_SET_500,
+	MMC5603NJ_PRD_SET_1000,
+	MMC5603NJ_PRD_SET_2000,
+
+	MMC5603NJ_PLANE_XY = 0,
+	MMC5603NJ_PLANE_XZ,
+	MMC5603NJ_PLANE_YZ,
+};
+
+void mmc5603nj_init(mmc5603nj_t *mag);
+void mmc5603nj_config_write(mmc5603nj_t *mag);
+uint16_t mmc5603nj_measure_req_raw(mmc5603nj_t *mag, uint8_t data_reg);
+float mmc5603nj_measure_req(mmc5603nj_t *mag, uint8_t data_reg);
+float mmc5603nj_measure_req_plane(mmc5603nj_t *mag, uint8_t plane);
+mmc5603nj_t *mmc5603nj_measure_req_alloc(int devaddr);
+void mmc5603nj_measure_req_free(mmc5603nj_t *req);
+
