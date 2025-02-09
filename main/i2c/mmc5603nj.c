b380de432ceba36573a3a91c8d5d78e0d0cb95f6
Zeke, KJ7NLL <kj7nll@gmail.com>
2025-02-09 13:43:25 -0800

    Corrected copyright header
    I wrote these files myself

--- main/i2c/mmc5603nj.c
+++ main/i2c/mmc5603nj.c
@@ -12,7 +12,7 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // 
-//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL and Eric Wheeler, KJ7LNW.
+//  Copyright (C) 2022- by Ezekiel Wheeler, KJ7NLL.
 //  All rights reserved.
 //
 //  The official website and doumentation for space-ham is available here:
